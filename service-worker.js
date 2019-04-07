78f8fb83e1b04cd101c266ecaafc39f3a20653b7
gordanakis <ncgordan@gmail.com>
2019-04-07 20:17:20 +0200

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -10,3 +10,31 @@ workbox.routing.registerRoute(
 	new RegExp('.*\.js'),
 	new workbox.strategies.NetworkFirst()
 );
+
+workbox.routing.registerRoute(
+  // Cache HTML files.
+  /\.html$/,
+  // Use cache but update in the background.
+  new workbox.strategies.StaleWhileRevalidate({
+    // Use a custom cache name.
+    cacheName: 'html-cache',
+  })
+);
+
+workbox.routing.registerRoute(
+  // Cache image files.
+  /\.(?:png|jpg|jpeg|svg|gif)$/,
+  // Use the cache if it's available.
+  new workbox.strategies.CacheFirst({
+    // Use a custom cache name.
+    cacheName: 'image-cache',
+    plugins: [
+      new workbox.expiration.Plugin({
+        // Cache only 20 images.
+        maxEntries: 20,
+        // Cache for a maximum of a week.
+        maxAgeSeconds: 7 * 24 * 60 * 60,
+      })
+    ],
+  })
+);
