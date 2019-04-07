5428d29dc9d873d4b727bb23893cad0d57621f26
gordanakis <ncgordan@gmail.com>
2019-04-07 20:07:37 +0200

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -4,6 +4,13 @@ importScripts('https://storage.googleapis.com/workbox-cdn/releases/4.1.1/workbox
 
 if (workbox) {
   console.log(`Yay! Workbox is loaded 🎉`);
+  
+  
+  workbox.routing.registerRoute(
+	  new RegExp('.*\.js'),
+	  new workbox.strategies.NetworkFirst()
+	);
+  
 } else {
   console.log(`Boo! Workbox didn't load 😬`);
 }
